29e27c069094d993e98f612df8bcfcd530a39c7c
Rajesh Parab <mailrajeshparab@gmail.com>
2016-06-25 22:31:51 +0530

    16. artisan-make-question-controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,5 @@ Route::get('welcome', function () {
 // });
 
 Route::get('about','PageController@about');
+
+Route::get('questions/{question}','QuestionController@show');
---
+++ resources/views/questions/show.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>show questions page</title>
+</head>
+<body>
+	<h1>show questions page</h1>
+	<p>more coming soon.....</p>
+</body>
+</html>
